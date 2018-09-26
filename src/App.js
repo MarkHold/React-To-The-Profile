f4064205cfadeab8a18735ce71c14d529d0c9ef3
markus lycnhold <ma223ik@gmail.com>
2018-09-26 22:19:37 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+
+        <div className="container">
+            <header className="blog-header py-3">
+                <div className="row flex-nowrap justify-content-between align-items-center">
+                    <div className="col-4 pt-1">
+                        <a className="btn btn-sm btn-outline-secondary" href="#">Subscribe</a>
+                    </div>
+                    <div className="col-4 text-center">
+                        <a className="blog-header-logo text-dark" href="#"> LYKONHOLD</a>
+                    </div>
+                    <div className="col-4 d-flex justify-content-end align-items-center">
+                        <a className="text-muted" href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
+                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                 stroke-linejoin="round" className="mx-3">
+                                <circle cx="10.5" cy="10.5" r="7.5"></circle>
+                                <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+                            </svg>
+                        </a>
+                        <a className="btn btn-sm btn-outline-secondary" href="#">Log in</a>
+                    </div>
+                </div>
+            </header>
+
+            <div className="nav-scroller py-1 mb-2">
+                <nav className="nav d-flex justify-content-between">
+                    <a className="p-2 text-muted" href="#">World</a>
+                    <a className="p-2 text-muted" href="#">U.S.</a>
+                    <a className="p-2 text-muted" href="#">Technology</a>
+                    <a className="p-2 text-muted" href="#">Design</a>
+                    <a className="p-2 text-muted" href="#">Culture</a>
+                    <a className="p-2 text-muted" href="#">Business</a>
+                    <a className="p-2 text-muted" href="#">Politics</a>
+                    <a className="p-2 text-muted" href="#">Opinion</a>
+                    <a className="p-2 text-muted" href="#">Science</a>
+                    <a className="p-2 text-muted" href="#">Health</a>
+                    <a className="p-2 text-muted" href="#">Style</a>
+                    <a className="p-2 text-muted" href="#">Travel</a>
+                </nav>
+            </div>
+
+            <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+                <div className="col-md-6 px-0">
+                    <form role="form" id="form-buscar" action="company.html">
+                        <div className="form-group">
+                            <div className="input-group">
+                                <input id="1" className="form-control" type="text" name="search"
+                                       placeholder="Company name..."/>
+                                <span className="input-group-btn">
+                           <button className="btn btn-success" type="submit">
+                             <i className="glyphicon glyphicon-search" aria-hidden="true"></i> Search</button>
+                        </span>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+          </div>
+
+    );
+  }
+}
+
+export default App;
