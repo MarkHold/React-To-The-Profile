40f4d687a6d1040adb1ec68a5ae7c60e5be1bf1c
markhold <ma223ik@gmail.com>
2018-10-15 12:01:44 +0200

    k

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
                         <a className="btn btn-sm btn-outline-secondary" href="#">Subscribe</a>
                     </div>
                     <div className="col-4 text-center">
-                        <a className="blog-header-logo text-dark" href="#"> LYKONHOLD</a>
+                        <a class="blog-header-logo text-dark" href="#"> LYKONHOLD</a>
                     </div>
                     <div className="col-4 d-flex justify-content-end align-items-center">
                         <a className="text-muted" href="#">
@@ -45,25 +45,44 @@ class App extends Component {
                 </nav>
             </div>
 
-            <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-                <div className="col-md-6 px-0">
-                    <form role="form" id="form-buscar" action="company.html">
-                        <div className="form-group">
-                            <div className="input-group">
-                                <input id="1" className="form-control" type="text" name="search"
-                                       placeholder="Company name..."/>
-                                <span className="input-group-btn">
-                           <button className="btn btn-success" type="submit">
-                             <i className="glyphicon glyphicon-search" aria-hidden="true"></i> Search</button>
-                        </span>
-                            </div>
-                        </div>
-                    </form>
+            <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+                <div class="col-md-6 px-0">
+                    <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
+                    <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+                    <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
                 </div>
             </div>
 
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div class="card-body d-flex flex-column align-items-start">
+                            <strong class="d-inline-block mb-2 text-primary">World</strong>
+                            <h3 class="mb-0">
+                                <a class="text-dark" href="#">Featured post</a>
+                            </h3>
+                            <div class="mb-1 text-muted">Nov 12</div>
+                            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#">Continue reading</a>
+                        </div>
+                        <img class="card-img-right flex-auto d-none d-md-block" data-src="weed.jpg" alt="Card image cap"></img>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="card flex-md-row mb-4 box-shadow h-md-250">
+                        <div class="card-body d-flex flex-column align-items-start">
+                            <strong class="d-inline-block mb-2 text-success">Design</strong>
+                            <h3 class="mb-0">
+                                <a class="text-dark" href="#">Post title</a>
+                            </h3>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#">Continue reading</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
           </div>
-
     );
   }
 }
